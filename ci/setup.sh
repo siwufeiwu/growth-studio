465e9836e053537936914bb5ccc8f45ad1830fc8
siwufeiwu <shellsiwufeiwu@163.com>
2017-04-28 14:54:54 +0800

    qq

--- ci/setup.sh
+++ ci/setup.sh
@@ -2,7 +2,7 @@
 PATH=$WORKSPACE/py35env/bin:/usr/local/bin:$PATH
 echo $(hostname)
 if [ ! -d "py35env" ]; then
-  virtualenv --distribute -p python py35env
+  /home/bjzhangjiwei/local/python-2.7.10/bin/virtualenv --distribute -p python py35env
 fi
 . py35env/bin/activate
 pip install fabric3
