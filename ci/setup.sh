e0ccd0d7982190a87589d7bcd78c8e0a149baee3
siwufeiwu <shellsiwufeiwu@163.com>
2017-04-28 14:44:16 +0800

    modify

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 PATH=$WORKSPACE/py35env/bin:/usr/local/bin:$PATH
 if [ ! -d "py35env" ]; then
-  virtualenv --distribute -p /usr/local/bin/python3.5 py35env
+  virtualenv --distribute -p python py35env
 fi
 . py35env/bin/activate
-pip3 install fabric3
-pip3 install -r requirements/dev.txt
+pip install fabric3
+pip install -r requirements/dev.txt
 
